a8bb100a2d85e8b707cf134c285f646fa4d16623
happycrazy198 <78469642+happycrazy198@users.noreply.github.com>
2025-01-29 12:22:44 +0800

    Update load.lua

--- load.lua
+++ load.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/happycrazy198/test/main/refs/heads/run.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/happycrazy198/test/refs/heads/main/run.lua"))()
