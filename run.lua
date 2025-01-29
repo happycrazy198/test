bc1919b0cfc9f94d905758faa32e883fea3b4ac8
happycrazy198 <78469642+happycrazy198@users.noreply.github.com>
2025-01-29 12:14:54 +0800

    Update run.lua

--- run.lua
+++ run.lua
@@ -2,7 +2,7 @@ print("running")
 local ui = Instance.new("ScreenGui",game.Players.LocalPlayer.PlayerGui)
 
 task.spawn(function()
-local frame = Instance.new("Frame")
+local frame = Instance.new("Frame",ui)
 wait(1)
 frame:Destroy()
 end)
