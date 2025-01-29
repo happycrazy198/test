79b244680f530cbb6d5938c619177770fd6b9727
happycrazy198 <78469642+happycrazy198@users.noreply.github.com>
2025-01-29 12:29:56 +0800

    Update run.lua

--- run.lua
+++ run.lua
@@ -3,6 +3,7 @@ local ui = Instance.new("ScreenGui",game.Players.LocalPlayer.PlayerGui)
 
 task.spawn(function()
 local frame = Instance.new("Frame",ui)
-wait(1)
+frame.Position = Udim2.new(0.5,0,0.5,0)
+wait(3)
 frame:Destroy()
 end)
